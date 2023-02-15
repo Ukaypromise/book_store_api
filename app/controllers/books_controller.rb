8fea63ee28d72c0d8fd9aa492ec248fd690a11ae
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-02-15 10:09:33 +0100

    Last code on a windows

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  def index
+    render json: Book.all
+  end
+
+  def create
+    render json: Book.create(book_params)
+  end
+
+
+
+
+  def book_params
+    params.require(:book).permit(:title, :author ) # add :genre, :price, :published_date, to the list of permitted params
+  end
+end
