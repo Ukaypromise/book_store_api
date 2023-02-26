bd6ec74b07b5dca3880689ce8681601b7ade2a54
Ukaypromise <promiseukaimo@gmail.com>
2023-02-26 18:54:05 +0100

    Add jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
     resources :books, only: [:index, :create]
     root to: "books#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_142101) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_172446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_142101) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "expired_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
 end
